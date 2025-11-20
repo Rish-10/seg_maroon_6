5e72481900f9e42ca4e07af5f91f1c79619200e5
Rishi <112700652+Rish-10@users.noreply.github.com>
2025-11-20 21:58:01 +0000

    created recipe.py and recipe_form.py

---
+++ team_maroon_6_2025/recipes/models/recipe.py
@@ -0,0 +1,18 @@
+from django.conf import settings 
+from django.db import models
+
+class Recipe(models.Model): 
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='recipes'
+    )
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    ingredients = models.TextField(help_text="Have one ingredient per line.")
+    instructions = models.TextField() 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self): 
+        return self.title
